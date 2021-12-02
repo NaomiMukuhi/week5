e65561a009d15c874b88cfb63b7852542fbca85c
NaomiMukuhi <naomi.mukuhi@student.moringaschool.com>
2021-12-02 12:36:52 +0300

    changes made commit

--- JS/script.js
+++ JS/script.js
@@ -18,3 +18,19 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
+function myFunction() {
+var x = document.getElementById("myTopnav");
+if (x.className === "topnav") {
+  x.className += " responsive";
+} else {
+  x.className = "topnav";
+}
+}
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
